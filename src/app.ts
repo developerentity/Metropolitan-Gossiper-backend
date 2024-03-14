3969f8c23abc221019f90e33098df39ad14ef6b7
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-03-14 17:46:41 +0200

    refactor: del red code

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,6 @@ export const app = express();
 app.use(express.json());
 // app.use(cookieParser());
 
-// app.use("/", (req, res) => res.json("Here is the get route"));
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/gossips", gossipsRouter);
