8e29d5448e7b3624c58deef4ba82bca1ccb1e6ae
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-03-14 16:50:45 +0200

    fix: add "/" route to the app

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ export const app = express();
 app.use(express.json());
 // app.use(cookieParser());
 
+app.use("/", (req, res) => res.json("Here is the get route"));
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/gossips", gossipsRouter);
