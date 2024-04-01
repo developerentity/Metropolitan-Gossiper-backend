93e88afa2b8e61a45dc5271f0b9b690f08d3c3e1
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-04-01 20:27:24 +0300

    WIP: refresh token ability

---
+++ src/middlewares/authenticate.ts
@@ -0,0 +1,51 @@
+import { NextFunction, Request, Response } from "express";
+import { HTTP_STATUSES } from "../http-statuses";
+import { jwtService } from "../application/jwt-service";
+import { MAX_TOKEN_AGE } from "../config";
+
+export const authenticate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const accessToken = req.headers.authorization?.split(" ")[1];
+  const refreshToken = req.cookies.refreshToken;
+  try {
+    if (!accessToken && !refreshToken) {
+      return res
+        .status(HTTP_STATUSES.UNAUTHORIZED_401)
+        .send("Access Denied. No token provided.");
+    }
+
+    if (!accessToken) {
+      throw new Error();
+    }
+
+    const decoded = await jwtService.getUserIdByToken(accessToken);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    if (!refreshToken) {
+      return res
+        .status(HTTP_STATUSES.UNAUTHORIZED_401)
+        .send("Access Denied. No refresh token provided.");
+    }
+
+    try {
+      const decoded = await jwtService.verifyRefreshJWT(refreshToken);
+      const accessToken = await jwtService.createJWT(decoded, +MAX_TOKEN_AGE!);
+
+      res
+        .status(HTTP_STATUSES.OK_200)
+        .cookie("refreshToken", refreshToken, {
+          httpOnly: true,
+          sameSite: "strict",
+          maxAge: +MAX_TOKEN_AGE! * 1000,
+        })
+        .header("Authorization", accessToken)
+        .send(decoded);
+    } catch (error) {
+      return res.status(HTTP_STATUSES.BAD_REQUEST_400).send("Invalid Token.");
+    }
+  }
+};
