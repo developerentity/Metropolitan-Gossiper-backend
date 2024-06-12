ab7ffc38d0672c21d3a83ab6676a4e3b9111b68d
Bohdan Kryzhnii <bohdan.kryzhnii@appvales.com>
2024-06-12 19:30:53 +0300

    refactor: auth.spec file

--- tests/integration/auth.spec.ts
+++ tests/integration/auth.spec.ts
@@ -14,61 +14,56 @@ describe("POST - /account/auth/signup", () => {
   afterAll(async () => {
     await db.closeDatabase();
   });
+  describe("When data is valid", () => {
+    it("should create a new user and return tokens", async () => {
+      const res = await request(app).post("/account/auth/signup").send(user1);
 
-  it("should return 400 code http response and 6 errors", async () => {
-    const res = await request(app).post("/account/auth/signup").send({});
+      expect(res.status).toBe(HTTP_STATUSES.OK_200);
 
-    expect(res.status).toBe(HTTP_STATUSES.BAD_REQUEST_400);
-    expect(res.body.errors.length).toEqual(6);
+      expect(res.body).toHaveProperty("registeredUser");
+      expect(res.body.registeredUser.firstName).toBe(user1.firstName);
+      expect(res.body.registeredUser.lastName).toBe(user1.lastName);
+      expect(res.body.registeredUser.email).toBe(user1.email);
+      expect(res.body.registeredUser).toHaveProperty("createdAt");
+      expect(res.body.registeredUser).toHaveProperty("id");
+
+      expect(res.body).toHaveProperty("backendTokens");
+      expect(res.body.backendTokens).toHaveProperty("accessToken");
+      expect(res.body.backendTokens).toHaveProperty("refreshToken");
+      expect(res.body.backendTokens).toHaveProperty("expiresIn");
+    });
   });
 
-  it("should create a new user and return tokens", async () => {
-    const newUser = {
-      firstName: "John",
-      lastName: "Doe",
-      email: "john.doe@example.com",
-      password: "password123",
-    };
+  describe("When data is invalid", () => {
+    it("should return 400 code http response and 6 errors", async () => {
+      const res = await request(app).post("/account/auth/signup").send({});
 
-    const res = await request(app).post("/account/auth/signup").send(newUser);
+      expect(res.status).toBe(HTTP_STATUSES.BAD_REQUEST_400);
+      expect(res.body.errors.length).toEqual(6);
+    });
 
-    expect(res.status).toBe(HTTP_STATUSES.OK_200);
+    it("should return 400 if email is already registered", async () => {
+      await request(app).post("/account/auth/signup").send(user1);
+      const res = await request(app).post("/account/auth/signup").send(user2);
 
-    expect(res.body).toHaveProperty("registeredUser");
-    expect(res.body.registeredUser.firstName).toBe(newUser.firstName);
-    expect(res.body.registeredUser.lastName).toBe(newUser.lastName);
-    expect(res.body.registeredUser.email).toBe(newUser.email);
-    expect(res.body.registeredUser).toHaveProperty("createdAt");
-    expect(res.body.registeredUser).toHaveProperty("id");
-
-    expect(res.body).toHaveProperty("backendTokens");
-    expect(res.body.backendTokens).toHaveProperty("accessToken");
-    expect(res.body.backendTokens).toHaveProperty("refreshToken");
-    expect(res.body.backendTokens).toHaveProperty("expiresIn");
-  });
-
-  it("should return 400 if email is already registered", async () => {
-    const existedUser = {
-      firstName: "Jane",
-      lastName: "Doe",
-      email: "jane.doe@example.com",
-      password: "password123",
-    };
-
-    await request(app).post("/account/auth/signup").send(existedUser);
-
-    const newUser = {
-      firstName: "Jane",
-      lastName: "Doe",
-      email: "jane.doe@example.com",
-      password: "password123",
-    };
-
-    const res = await request(app).post("/account/auth/signup").send(newUser);
-
-    expect(res.status).toBe(HTTP_STATUSES.BAD_REQUEST_400);
-    expect(res.body).toEqual({
-      message: "User with such email already registered",
+      expect(res.status).toBe(HTTP_STATUSES.BAD_REQUEST_400);
+      expect(res.body).toEqual({
+        message: "User with such email already registered",
+      });
     });
   });
 });
+
+const user1 = {
+  firstName: "Jane",
+  lastName: "Doe",
+  email: "jane.doe@example.com",
+  password: "password123",
+};
+
+const user2 = {
+  firstName: "Jane",
+  lastName: "Doe",
+  email: "jane.doe@example.com",
+  password: "password123",
+};
